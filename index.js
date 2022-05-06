ed383f7f956ae728a1f58f5d7fb8c43e46c957ac
Juanse-P <jspp020205@gmail.com>
2022-05-05 21:16:12 -0500

    Primera entrega del proyecto final

--- index.js
+++ index.js
@@ -1,215 +1,150 @@
-//Se declaran los array que se usan en el "carrito de compras", que amedida que se añadan productos se van llenando
-let resultadoCarritos = [];
-
-let carritoProductoFinal = []
-// se crean los objetos de los productos y las funciones que se usan cuando el usuario selecciona alguna opción 
-class misProductos {
-    constructor(tipo, marca, modelo, precioString, precioUni) {
-        this.tipo = tipo;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precioString = precioString;
-        this.precioUni = precioUni;
-    }
-    agregarCarrito() {
-        alert(`Se a añadido ${this.marca} ${this.modelo} a tu carrito`);
-    }
-    agregarPrecioAlCarrito() {
-        return resultadoCarritos.push(this.precioUni);
-    }
-    escribirLasOpcionesYCarritoProductoFinal() {
-        return `${this.marca} ${this.modelo}    ${this.precioString}  `;
-    }
+//Se declaran las variables, los array, los objetos dentro de los array y las funciones que permiten crear tanto los nombres de los productos como permite pushear los datos a los arregos
+let resultadoCarrito = []
+let resumenCompra = []
+let arrayProductos = [
+    { tipo: `Teclado`, marca: `Logitech`, modelo: `G915`, precioString: `$ 820.000`, precioUni: 820000 },
+    { tipo: `Teclado`, marca: `Logitech`, modelo: `K400`, precioString: `$ 129.000`, precioUni: 129000 },
+    { tipo: `Teclado`, marca: `Kumara`, modelo: `OUTEMO`, precioString: `$ 170.000`, precioUni: 170000 },
+    { tipo: `Mouses`, marca: `Logitech`, modelo: `G502`, precioString: `$ 215.000`, precioUni: 215000 },
+    { tipo: `Mouses`, marca: `Razer`, modelo: `Viper mini`, precioString: `$ 140.000`, precioUni: 140000 },
+    { tipo: `Mouses`, marca: `Redragon`, modelo: `Griffin M607`, precioString: `$ 70.900`, precioUni: 70900 },
+    { tipo: `Mousepad`, marca: `KlipXtreme`, modelo: `mousepad LP82`, precioString: `$ 80.000`, precioUni: 80000 },
+    { tipo: `Mousepad`, marca: `Aoas`, modelo: `S4000`, precioString: `$ 50.000`, precioUni: 50000 },
+    { tipo: `Mousepad`, marca: `Genius`, modelo: `G WMP 200M`, precioString: `$50.000`, precioUni: 50000 }
+]
+function escribirDentroProductos(posArray) {
+    return `${arrayProductos[posArray].marca} ${arrayProductos[posArray].modelo}  ${arrayProductos[posArray].precioString}`;
 }
-
-const teclado1 = new misProductos(`teclados`, `Logitech`, `G915 TKL`, `$ 820.000`, 820000);
-const teclado2 = new misProductos(`teclado`, `Logitech`, `K400`, `$ 129.000`, 129000);
-const teclado3 = new misProductos(`teclado`, `Kumara`, `OUTEMO`, `$ 170.000`, 170000);
-const mouse1 = new misProductos(`mouse`, `Logitech`, `G502`, `$ 215.000`, 215000);
-const mouse2 = new misProductos(`mouse`, `Razer`, `Viper mini`, `$ 140.000`, 140000);
-const mouse3 = new misProductos(`mouse`, `Redragon`, `Griffin M607`, `$ 70.000`, 70900);
-const mousepad1 = new misProductos(`mousepad`, `KlipXtreme`, `mousepad LP82`, `$ 80.000`, 80000);
-const mousepad2 = new misProductos(`mousepad`, `Aoas`, `S4000`, `$ 50.000`, 50000);
-const mousepad3 = new misProductos(`mousepad`, `Genius`, `G WMP 200M`, `$ 50.000`, 50000);
-
-let miCarrito;
-let entrada = true;
-let productos;
-let menuPre;
-
-let primerMenu = "SHORPAD Tienda de componentes Externos para PC" +
-    "\n" +
-    "¿Que producto estas interesado en adquirir?" +
-    "\n" +
-    "1 - Teclados" +
-    "\n" +
-    "2 - Mouse" +
-    "\n" +
-    "3 - Mousepad" +
-    "\n" +
-    "\n" +
-    "4 - Carrito" +
-    "\n" +
-    "5 - Salir";
-//Todo el codigo se basa en condicionales, como if - else if - switch.
-while (entrada) {
-    productos = prompt(primerMenu);
-    if (productos == "1") {
-
-        menuPre = true;
-
-        while (menuPre) {
-            let teclados = "Teclados disponibles:" +
-                "\n" +
-                `1 - ${teclado1.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `2 - ${teclado2.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `3 - ${teclado3.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                "4 - Regresar";
-            secTeclados = prompt(teclados);
-
-            switch (secTeclados) {
+function alertaAnidado(posArray) {
+    return alert(`Se ha añadido ${escribirDentroProductos(posArray)}   a tu carrito`);
+}
+function subirPrecio(posArray) {
+    return resultadoCarrito.push(arrayProductos[posArray].precioUni);
+}
+function subirProducto(posArray) {
+    return resumenCompra.push(`${escribirDentroProductos(posArray)}`);
+}
+function dentroProductos(posArray) {
+    alertaAnidado(posArray);
+    subirPrecio(posArray);
+    subirProducto(posArray);
+}
+let carroDeCompras;
+let entrar = true;
+let eleccionMenu;
+let preMenu;
+//Todo el codigo se basa en condicionales, como el uso de if o de switch
+while (entrar) {
+    eleccionMenu = prompt(`SHORPAD Tienda de componentes externos para PC \n \n ¿Que producto esta interesado en adquirir? \n 1 - Teclados \n 2 - Mouse \n 3 - Mousepad \n \n 4 - Filtrar \n 5 - Carrito \n 6 o cualquier otro numero - Salir`);
+    preMenu = true;
+    if (eleccionMenu == "1") {
+        preMenu = true;
+        while (preMenu) {
+            let teclados = prompt(`Los teclados disponibles son: \n \n 1 - ${escribirDentroProductos(0)} \n 2 - ${escribirDentroProductos(1)} \n 3 - ${escribirDentroProductos(2)} \n \n 4 - Salir`);
+            seccionTeclados = teclados;
+            switch (seccionTeclados) {
                 case "1":
-                    teclado1.agregarCarrito();
-                    teclado1.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${teclado1.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(0);
                     break;
                 case "2":
-                    teclado2.agregarCarrito();
-                    teclado2.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${teclado2.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(1);
                     break;
                 case "3":
-                    teclado3.agregarCarrito();
-                    teclado3.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${teclado3.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(2);
                     break;
                 case "4":
-                    menuPre = false;
+                    preMenu = false;
                 default:
                     break;
             }
         }
-    }
-    else if (productos == "2") {
-        salirTec = true;
-        while (salirTec) {
-            let mouses = "Mouses disponibles" +
-                "\n" +
-                `1 - ${mouse1.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `2 - ${mouse2.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `3 - ${mouse3.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                "4 - Regresar";
-            secMouses = prompt(mouses);
-            switch (secMouses) {
+    } else if (eleccionMenu == "2") {
+        entradaMouse = true;
+        while (entradaMouse) {
+            let mouses = prompt(`Los mouses disponibles son: \n \n 1 - ${escribirDentroProductos(3)} \n 2 - ${escribirDentroProductos(4)} \n 3 - ${escribirDentroProductos(5)} \n \n 4 - Salir`);
+            seccionMouses = mouses;
+            switch (seccionMouses) {
                 case "1":
-                    mouse1.agregarCarrito();
-                    mouse1.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mouse1.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(3);
                     break;
                 case "2":
-                    mouse2.agregarCarrito();
-                    mouse2.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mouse2.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(4);
                     break;
                 case "3":
-                    mouse3.agregarCarrito();
-                    mouse3.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mouse3.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(5);
                     break;
                 case "4":
-                    salirTec = false;
+                    entradaMouse = false;
                 default:
                     break;
             }
         }
-    } else if (productos == "3") {
-        salirMou = true;
-        while (salirMou) {
-            let mousepad = "Mouse Pad Disponibles" +
-                "\n" +
-                `1 - ${mousepad1.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `2 - ${mousepad2.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                `3 - ${mousepad3.escribirLasOpcionesYCarritoProductoFinal()}` +
-                "\n" +
-                "4 - Regresar";
-            secMousepad = prompt(mousepad);
-
-            switch (secMousepad) {
+    } else if (eleccionMenu == "3") {
+        entradaMousepad = true;
+        while (entradaMousepad) {
+            let mousepads = prompt(`Los mousepad disponibles son: \n \n 1 - ${escribirDentroProductos(6)} \n 2 - ${escribirDentroProductos(7)} \n 3 - ${escribirDentroProductos(8)} \n \n 4 - Salir`);
+            seccionMousepad = mousepads;
+            switch (seccionMousepad) {
                 case "1":
-                    mousepad1.agregarCarrito();
-                    mousepad1.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mousepad1.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(6);
                     break;
                 case "2":
-                    mousepad2.agregarCarrito();
-                    mousepad2.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mousepad2.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(7);
                     break;
                 case "3":
-                    mousepad3.agregarCarrito();
-                    mousepad3.agregarPrecioAlCarrito();
-                    carritoProductoFinal.push(`${mousepad3.escribirLasOpcionesYCarritoProductoFinal()}`);
+                    dentroProductos(8);
                     break;
                 case "4":
-                    salirMou = false;
+                    entradaMousepad = false;
                 default:
                     break;
             }
         }
-    } else if (productos == "4") {
+    } else if (eleccionMenu == "4") {
+        entradaFiltro = true;
+        while (entradaFiltro) {
+            let filtro = prompt(`Puedes filtrar por 1 criterio, poniendo la marca del elemento que buscas \n 1 - Filtrar \n \n 2 - Salir`);
+            secFiltro = filtro;
+            switch (secFiltro) {
+                case "1":
+                    let filtro1 = prompt(`Escribe el filtro que quieres aplicar (Logitech, Redragon, Genius, etc). En caso de que la alerta salga vacia significa que no hay productos con esta caracteristica.`)
+                    filtrado = filtro1;
+                    let primerFiltro = arrayProductos.filter((ele) => ele.marca === filtrado)
+                    str = JSON.stringify(primerFiltro, null, 4);
+                    alert(str)
+                    console.log(primerFiltro);
 
-        let sumarResultado = 0
-
-        for (let i = 0; i < resultadoCarritos.length; i++) {
-            sumarResultado += resultadoCarritos[i];
+                    break;
+                case "2":
+                    entradaFiltro = false;
+                default:
+                    break;
+            }
         }
-
-        if (sumarResultado > 0) {
-            let carritoCompras = "Mi carrito" +
-                "\n" +
-                `${carritoProductoFinal.join(",  ")}` +
-                "\n" +
-                "\n" +
-                "Su cuenta total es de:   $" +
-                `${sumarResultado}` +
-                "cop" +
-                "\n" +
-                "\n" +
-                "1 - Pagar" +
-                "\n" +
-                "2 - Salir del carrito";
-
-            miCarrito = prompt(carritoCompras);
-
+    } else if (eleccionMenu == "5") {
+        let sumarTotal = 0
+        for (let i = 0; i < resultadoCarrito.length; i++) {
+            sumarTotal += resultadoCarrito[i];
+        }
+        if (sumarTotal > 0) {
+            let carritoDeCompras = prompt(`Mi carrito: \n ${resumenCompra.join("\n")} \n \n La cuenta total es de:  $ ${sumarTotal} cop \n \n 1 - pagar \n 2 - Abandonar carrito`);
+            miCarrito = carritoDeCompras;
             switch (miCarrito) {
                 case "1":
-                    alert("El precio total de la factura fue cargado a la cuenta bancaria:" +
-                        "\n" +
-                        "****.****.****.**51" +
-                        "\n" +
-                        "\n" +
-                        "¡Gracias por su compra! Esperamos verle pronto de nuevo en nuestra pagina.");
-                    entrada = false;
+                    alert(`El precio total de la factura due cargado a la cuenta bancaria: \n ****.****.****.**51 \n \n ¡Gracias por su compra! Esperamos verle pronto de nuevo en nuestra pagina!`);
+                    entrar = false;
                     break;
                 case "2":
-                    menuPre = false;
+                    preMenu = false;
                     break;
                 default:
                     break;
             }
         } else {
-            alert("¡Parece que olvidaste poner productos en tu carrito!");
+            alert(`¡Parece que olvidaste poner productos en tu carrito!`);
         }
     } else {
-        entrada = false;
-        alert("Ha salido de nuestra pagina, ¡vuelva pronto!")
+        entrar = false;
+        alert(`Ha salido de nuestra pagina, ¡Vuelva pronto!`)
     }
 }
